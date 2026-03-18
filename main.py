3ab207f81608074e0c728297b4503304a653c41a
dssserate-diamondgem-2031 <dssserate@cmc.pshs.edu.ph>
2026-03-18 10:36:14 +0800

    Implement name input and birth year calculation
    Added functionality to calculate birth year based on age input.

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+### v1.0.0 - 03/18/2026 - Added name input. 
 name = input("Name? ")
-print("Hello" + name)
+print("Hello" + name) 
+
+### v1.2.0 - 03/18/2026 - Added birth math.
+year = 2026 - int(age)
+print("Born in: " + str(year))
