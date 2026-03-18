44291d1f5fd0da471801670cc09dafde0aa757ca
dssserate-diamondgem-2031 <dssserate@cmc.pshs.edu.ph>
2026-03-18 10:39:08 +0800

    Removed duplicate version 1.0.0 entry in main.py

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
 ### v1.0.0 - 03/18/2026 - Added name input. 
 name = input("Name? ")
-print("Hello" + name) 
-
-### v1.2.0 - 03/18/2026 - Added birth math.
-year = 2026 - int(age)
-print("Born in: " + str(year))
+print("Hello" + name)
